876b758b2ee3b70163d7d27df9e8a71b8ca4d264
Benjamin Carhart <felicity.happenstance@gmail.com>
2016-12-31 01:18:43 -0500

    Corrected some common typos
    Mixed up my "n" and "s" in the first response call.  Also forgot an "e".
    Since I'm typing it alacarte in Github directly, not all that shocking!

--- hellohtp.js
+++ hellohtp.js
@@ -7,11 +7,11 @@
  
  //use more Node.js http functionality to create a listening server...
  http.createServer(function (request, response) {
-    resposne.writeHead(200, {'Content-Type': 'text/plain'});   //very basic HTML coded response
+    response.writeHead(200, {'Content-Type': 'text/plain'});   //very basic HTML coded response
     response.end('Hello World...\n\n\n ... and goodbye!\n');  //and the message within.
     } ).listen (listenPort);                                  //finish the createServer call by asking
                                                               //it to lisen at the listenPort
 
-consol.log('Server Started... open a web-browser and goto localhost:' + listenPort + ' to see if it works');
+console.log('Server Started... open a web-browser and goto localhost:' + listenPort + ' to see if it works');
 
 //end
